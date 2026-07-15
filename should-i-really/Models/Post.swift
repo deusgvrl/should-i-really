8435578306a11c8dd8b5aec78fe12c9d8e1118e9
Jose Taneo <joseperdanat@gmail.com>
2026-07-16 04:04:29 +0700

    feat: add scenario json and post model struct

--- should-i-really/Models/Post.swift
+++ should-i-really/Models/Post.swift
@@ -7,6 +7,64 @@
 
 import Foundation
 
+// MARK: - Jose (Model yang dipakai json)
+enum CropType: String, Codable {
+    case positive = "positive"
+    case negative = "negative"
+}
+
+enum QuadrantPosition: Int, Codable {
+    case topLeft = 1
+    case topRight = 2
+    case bottomLeft = 3
+    case bottomRight = 4
+}
+
+struct Comment: Identifiable, Codable, Equatable {
+    let id: String
+    let username: String
+    let text: String
+}
+
+struct CaptionOption: Identifiable, Codable, Equatable {
+    let id: String
+    let type: CropType
+    let text: String
+    let nextNodeId: String
+    let comments: [Comment]
+}
+
+struct CropDetails: Codable {
+    let quadrant: QuadrantPosition
+    let captions: [CaptionOption]
+}
+
+struct CropsContainer: Codable {
+    let positiveCrop: CropDetails
+    let negativeCrop: CropDetails
+    
+    // perlu mapping supaya nama variabel camelCase Swift bisa baca format snake_case dari JSON
+    enum CodingKeys: String, CodingKey {
+        case positiveCrop = "positive_crop"
+        case negativeCrop = "negative_crop"
+    }
+}
+
+struct ScenarioNode: Identifiable, Codable {
+    let id: String
+    let round: Int
+    let bigPictureId: String
+    let crops: CropsContainer
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case round
+        case bigPictureId = "big_picture_id"
+        case crops
+    }
+}
+
+// MARK: - Fany
 public enum Quadrant: Int, CaseIterable {
     case topLeft = 0
     case topRight = 1
