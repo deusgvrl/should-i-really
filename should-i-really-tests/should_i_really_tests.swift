026e9b00bc6d28466b50ff90b0223fa41061f998
febeflo <steffanyflorence@gmail.com>
2026-07-17 17:07:27 +0700

    feat: add unit test script

---
+++ should-i-really-tests/should_i_really_tests.swift
@@ -0,0 +1,51 @@
+//
+//  should_i_really_tests.swift
+//  should-i-really-tests
+//
+//  Created by Steffany Florence on 17/07/26.
+//
+
+import Testing
+@testable import should_i_really
+
+@MainActor
+struct CompletePostFlowTests {
+    
+    let gameViewModel: GameViewModel
+    let postCreationViewModel: PostCreationViewModel
+    
+    init() {
+        gameViewModel = GameViewModel()
+        gameViewModel.enterUsername("TestPlayer")
+        postCreationViewModel = PostCreationViewModel(gameViewModel: gameViewModel)
+    }
+    
+    @Test("User can select quadrant 1, pick a caption, and post with a single comment")
+    func completePostCreationAndCommentFlow() throws {
+        defer { gameViewModel.deleteActiveSave() }
+
+        let initialFeedCount = gameViewModel.feedPosts.count
+        let currentNode = try #require(gameViewModel.currentNode, "JSON must be loaded!")
+        #expect(currentNode.id == "1A")
+        
+        let selectedQuadrant = try #require(QuadrantPosition(rawValue: 1))
+        postCreationViewModel.selectedQuadrant = selectedQuadrant
+        postCreationViewModel.confirmPhotoSelection()
+        
+        let availableCaptions = postCreationViewModel.availableCaptions
+        let chosenCaption = try #require(availableCaptions.first)
+        
+        postCreationViewModel.selectCaption(chosenCaption)
+        postCreationViewModel.finalizeAndPost()
+        
+        let updatedFeed = gameViewModel.feedPosts
+        #expect(updatedFeed.count == initialFeedCount + 1)
+        
+        let newlyPublishedPost = try #require(updatedFeed.first)
+        
+        #expect(newlyPublishedPost.comments.id == chosenCaption.comments.id)
+        #expect(!newlyPublishedPost.comments.text.isEmpty)
+        
+        #expect(gameViewModel.currentNode?.id == chosenCaption.nextNodeId)
+    }
+}
