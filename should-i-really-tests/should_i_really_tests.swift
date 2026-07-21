08a3ef81ce76f86993f78f81ad763595f5f866fb
Jose Taneo <joseperdanat@gmail.com>
2026-07-21 13:34:10 +0700

    feat: add story tests

--- should-i-really-tests/should_i_really_tests.swift
+++ should-i-really-tests/should_i_really_tests.swift
@@ -51,3 +51,40 @@ struct CompletePostFlowTests {
         #expect(gameViewModel.currentNode?.id == chosenCaption.nextNodeId)
     }
 }
+
+@MainActor
+struct StoryGameViewModelTests {
+    let gameViewModel: GameViewModel
+    
+    init() {
+        gameViewModel = GameViewModel()
+        gameViewModel.enterUsername("TestPlayer")
+    }
+    
+    @Test("User can move to ending route when they reach the end of the story")
+    func testAdvanceStoryRoutesToEnding() throws {
+        defer { gameViewModel.deleteActiveSave() }
+        let captionOption: CaptionOption = CaptionOption(id: "1", type: .positive, text: "Caption", nextNodeId: "ENDING_A", comments: nil)
+        
+        gameViewModel.advanceStory(nextNodeId: "ENDING_A", chosenQuadrant: .bottomLeft, chosenCaption: captionOption)
+        
+        #expect(gameViewModel.currentRoute == .ending)
+    }
+    
+    @Test("User can view their post insights based on the cropped picture and caption they choose")
+    func testPostInsights() throws {
+        defer { gameViewModel.deleteActiveSave() }
+        
+        let node = try #require(gameViewModel.currentNode)
+        let positiveQuadrant = node.crops.positiveCrop.quadrant
+        let captionOption: CaptionOption = CaptionOption(id: "1A", type: .positive, text: "Caption", nextNodeId: "2B", comments: nil)
+        
+        gameViewModel.advanceStory(nextNodeId: node.id, chosenQuadrant: positiveQuadrant, chosenCaption: captionOption)
+        
+        let newestPost = try #require(gameViewModel.feedPosts.first)
+        
+        #expect(newestPost.photoGuardResult == .positive)
+        #expect(newestPost.vibeCheckResult == .positive)
+        
+    }
+}
